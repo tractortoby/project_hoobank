3708baf1a19d3291f414c45170f1bb26b141f39b
tractortoby <tobychung@duck.com>
2022-10-02 19:28:15 +0800

    Delete navLinks margin login and add space-x-9 in parent div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,13 @@ const Navbar = () => {
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
 
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1 space-x-9">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] ${
               active === nav.title ? "text-white" : "text-dimWhite"
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            }`}
             onClick={() => setActive(nav.title)}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
